b981384750685459982c8660a33daada0fac9df3
dronreef2 <202001579176@alunos.estacio.br>
2025-05-20 10:47:05 -0300

    exercicios 1, 2, 3

--- src/implementacao/1-retornar-medias.ts
+++ src/implementacao/1-retornar-medias.ts
@@ -1,3 +1,13 @@
-function retornarMedias(){
-    return null
+import { turma } from '../../listaPreConstruida';
+
+function retornarMedias(): number[] {
+    return turma.map(aluna => {
+        const { p1, p2, p3 } = aluna.prova;
+        const media = Number(((p1 + p2 + p3) / 3).toFixed(1));
+        return media;
+    });
 }
+
+console.log(retornarMedias());
+
+export default retornarMedias;
--- src/implementacao/3-retornar-reprovadas.ts
+++ src/implementacao/3-retornar-reprovadas.ts
@@ -1,3 +1,15 @@
-function retornarReprovadas() {
-    return null;
+import { turma } from '../../listaPreConstruida';
+
+function retornarAprovadas(): string[] {
+    return turma
+        .filter(aluna => {
+            const { p1, p2, p3 } = aluna.prova;
+            const media = (p1 + p2 + p3) / 3;
+            return media <= 7;
+        })
+        .map(aluna => aluna.nome);
 }
+
+console.log(retornarAprovadas());
+
+export default retornarAprovadas;
